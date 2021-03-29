3afcfeb9970c6f6c304e7c5bdb74868444c2f991
Alberto Aróstegui <aarosteguig@gmail.com>
2021-03-29 16:01:57 +0200

    prueba1

--- Ruleta/src/org/pmoo/ruleta/Banca.java
+++ Ruleta/src/org/pmoo/ruleta/Banca.java
@@ -4,12 +4,18 @@ public class Banca {
 	//Atributos
 	private static Banca miBanca = null;
 	
+	
 	//Constructora
 	private Banca() {
 		
 	}
 	//Métodos
 	public static Banca getMiBanca(){
-		
+		if (miBanca == null) {
+			miBanca = new Banca();
+		}
+		return miBanca;
 	}
+	//Alvaro la chupa biende
+	
 }
